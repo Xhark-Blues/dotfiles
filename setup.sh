a16f6ac5db168b69a1be7dbb3aef9d16205141c2
Ruben Paz <me@ruben.io>
2016-07-03 00:33:29 +0100

    Tmux & Vim sessions

--- setup.sh
+++ setup.sh
@@ -2,8 +2,19 @@
 
 CURRENT_DIR=`pwd -P`
 
-rm -fr ~/.vimrc
-ln -s $CURRENT_DIR/.vimrc ~/.vimrc
+rm -fr $HOME/.vimrc
+ln -s $CURRENT_DIR/.vimrc $HOME/.vimrc
+
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 
 rm -fr ~/.tmux.conf
-ln -s $CURRENT_DIR/.tmux.conf ~/.tmux.conf
+ln -s $CURRENT_DIR/.tmux.conf $HOME/.tmux.conf
+
+mkdir -p $HOME/.config/fish
+rm -fr $HOME/.config/fish/config.fish
+ln -s $CURRENT_DIR/config.fish $HOME/.config/fish/config.fish
+
+rm -fr $XDG_CONFIG_HOME/nvim
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+rm -fr $XDG_CONFIG_HOME/nvim/init.vim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
