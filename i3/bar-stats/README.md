500294af44f7abaef52e1ae4a514394a13e89ec1
Ruben Paz <me@ruben.io>
2017-06-06 14:48:34 +0100

    Switch to gnome-shell

--- i3/bar-stats/README.md
+++
@@ -1,2 +0,0 @@
-# ts-playground
-Typescript Playground
--- i3/bar-stats/src/time.ts
+++
@@ -1,8 +0,0 @@
-import * as moment from 'moment';
-
-export default async function time() {
-  const date = moment();
-
-  // f073 is a calendar glyph
-  return  `${date.format('dd DD HH:mm:ss')} \uf073`;
-}
